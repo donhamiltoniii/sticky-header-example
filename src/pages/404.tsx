1279771307d7d1d99bb1c244a81bac48f48711dc
Don Hamilton III <donhamiltoniii@gmail.com>
2020-01-13 22:24:38 -0500

    Save 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Layout from "../components/layout.component"
-import SEO from "../components/seo"
+import SEO from "../components/seo.components"
 
 const NotFoundPage = () => (
   <Layout>
